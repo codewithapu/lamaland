f60abd82846af5f866ee3733a258cb63ec01eab4
Supriya Mahato <codewithapu@gmail.com>
2023-10-04 01:58:07 +0530

    added metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Wrap N Go',
+  description: "Get What You're Craving",
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

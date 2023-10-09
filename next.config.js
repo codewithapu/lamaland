d12b469bdf654c08d6f5aee18776537d1f022bc0
Supriya Mahato <codewithapu@gmail.com>
2023-10-09 15:23:09 +0530

    added app feature

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
-/** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa');
 
-module.exports = {
+// const pwaConfiguration = {
+//   dest: 'public',
+//   disable: process.env.NODE_ENV === 'development',
+//   register: true,
+//   skipWaiting: true,
+// };
+
+// module.exports = withPWA({
+//   ...pwaConfiguration,
+//   images: {
+//     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+//   },
+// });
+
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+});
+
+const nextConfig = withPWA({
+  // next config
   images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
-}
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+  }
+});
+module.exports = nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,18 @@
 import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react';
 import { CartProvider } from '../context/CartContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+
       <CartProvider>
         <SessionProvider session={pageProps.session}>
           <Component {...pageProps} key={router.route} />
